cbc34c649176ab3e4b600d1bd1514aed8b9b700f
Chris Pennington <cpenn368@gmail.com>
2024-12-05 19:14:37 -0700

    day-1-part-1

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
-const list1 = [3, 4, 2, 1, 3, 3];
-const list2 = [4, 3, 5, 3, 9, 3];
+const file = Bun.file("./input.txt");
+
+const list1: number[] = [];
+const list2: number[] = [];
+
+const fileData = await file.text();
+
+fileData.split("\n").forEach((row, ind) => {
+  const [first, second] = row.split("   ");
+  list1.push(parseInt(first.trim()));
+  list2.push(parseInt(second.trim()));
+});
 
 const totalDistance = (arr1: number[], arr2: number[]) => {
   const sortedList2 = arr2.toSorted();
-  return arr1
-    .toSorted()
-    .reduce((acc, num, idx) => acc + sortedList2[idx] - num, 0);
+  return arr1.toSorted().reduce((acc, num, idx) => {
+    return acc + Math.abs(sortedList2[idx] - num);
+  }, 0);
 };
 
 console.log(totalDistance(list1, list2));
