6a35ec95fc416bb0c91146fa5f09fbdfffaf2f10
Chris Pennington <cpenn368@gmail.com>
2024-12-10 20:03:06 -0700

    day-2-part-1

---
+++ 02/index.ts
@@ -0,0 +1,22 @@
+const file = Bun.file("02/input.txt");
+const rawData = await file.text();
+
+const data = rawData
+  .split("\n")
+  .map((row) => row.split(" ").map((i) => parseInt(i)));
+
+const numOfSafeReports = data.reduce((total, row) => {
+  if (row[0] === row[1]) {
+    return total;
+  }
+  const dir = row[0] - row[1] < 0 ? "inc" : "dec";
+
+  const passChecks = row.every((num, idx, arr) => {
+    if (idx === 0) return true;
+    if (dir === "inc" && num - arr[idx - 1] <= 0) return false;
+    if (dir === "dec" && arr[idx - 1] - num <= 0) return false;
+    return Math.abs(num - arr[idx - 1]) > 3 ? false : true;
+  });
+
+  return passChecks ? total + 1 : total;
+}, 0);
